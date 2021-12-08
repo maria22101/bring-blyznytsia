b2aed6155aa003bdb363409caa7b0077a13877e8
Mariia Bilous <Mariia_Bilous@epam.com>
2021-12-08 19:06:17 +0200

    implement @Bean annotation

--- src/main/java/com/blyznytsia/bring/demo/config1/DemoAppConfig1.java
+++ src/main/java/com/blyznytsia/bring/demo/config1/DemoAppConfig1.java
@@ -1,11 +1,18 @@
-package com.blyznytsia.bring.demo.config;
+package com.blyznytsia.bring.demo.config1;
 
+import com.blyznytsia.bring.context.annotation.Bean;
 import com.blyznytsia.bring.context.annotation.ComponentScan;
 import com.blyznytsia.bring.context.annotation.Configuration;
+import com.blyznytsia.bring.demo.classes_beans.ClassBean;
 
 @Configuration
 @ComponentScan({"com.blyznytsia.bring.demo.classes",
                 "com.blyznytsia.bring.demo.classes_x",
                 "com.blyznytsia.bring.demo.classes_y"})
-public class DemoAppConfig {
+public class DemoAppConfig1 {
+
+    @Bean
+    public ClassBean classBean() {
+        return new ClassBean();
+    }
 }
